b7ea3bdc5dbc663da811295ee9dfaee3ff900f1e
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-18 15:50:00 -0700

    drawsamples! => sample!

--- test/notebook.jl
+++ test/notebook.jl
@@ -12,12 +12,12 @@ t = as((x=asℝ, σ=asℝ₊))
 
 chain = initialize!(dynamicHMC(), ℓ, t)
 
-drawsamples!(chain, 9)
-drawsamples!(chain, 90)
+sample!(chain, 9)
+sample!(chain, 90)
 
 chains = initialize!(4, dynamicHMC(), ℓ, t)
 
-drawsamples!(chains, 9)
-drawsamples!(chains, 90)
+sample!(chains, 9)
+sample!(chains, 90)
 
 samples(chains)
