cab13e2aba6f09298900f18a715f0143686feae6
Seth Axen <seth.axen@gmail.com>
2021-07-14 11:53:49 +0200

    Add testsets

--- test/notebook.jl
+++ test/notebook.jl
@@ -2,6 +2,7 @@
 
 using SampleChainsDynamicHMC
 using TransformVariables
+using Test
 
 function ℓ(nt)
 	z = nt.x/nt.σ
@@ -10,14 +11,31 @@ end
 
 t = as((x=asℝ, σ=asℝ₊))
 
-chain = newchain(dynamichmc(), ℓ, t)
+@testset "single chain" begin
+	chain = newchain(dynamichmc(), ℓ, t)
+	@test chain isa SampleChainsDynamicHMC.DynamicHMCChain
+	@test length(chain) == 1
 
-sample!(chain, 9)
-sample!(chain, 90)
+	sample!(chain, 9)
+	@test length(chain) == 10
 
-chains = newchain(4, dynamichmc(), ℓ, t)
+	sample!(chain, 90)
+	@test length(chain) == 100
+end
 
-sample!(chains, 9)
-sample!(chains, 90)
+@testset "multichain" begin
+	chains = newchain(4, dynamichmc(), ℓ, t)
+	@test chains isa MultiChain
+	chains_chains = getfield(chains, :chains)
+	@test length(chains_chains) == 4
+	@test all(x -> length(x) == 1, chains_chains)
 
-samples(chains)
+	sample!(chains, 9)
+	@test all(x -> length(x) == 10, chains_chains)
+	sample!(chains, 90)
+	@test all(x -> length(x) == 100, chains_chains)
+
+	samples(chains)
+end
+
+
